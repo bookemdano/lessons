38ba60ad48fe8ec3be2c87e47675ca9d1db5d710
Dan <bookemdano@gmail.com>
2021-12-22 13:31:02 -0500

    Remove mockATM

--- Linear Algebra/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Linear Algebra/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace Linear_Algebra {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Linear Algebra;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Linear Algebra;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
