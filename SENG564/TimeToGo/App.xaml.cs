0d33ad0fb32070975ba1b21777e7ec51bfdda9b4
Dan <bookemdano@gmail.com>
2022-11-26 16:40:28 -0500

    First Post

---
+++ SENG564/TimeToGo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeToGo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SENG564/TimeToGo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeToGo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
