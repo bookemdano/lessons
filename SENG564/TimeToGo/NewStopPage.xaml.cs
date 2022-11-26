f574f2db9e5c65b69e12255dd8ad190188ccf7ae
Dan <bookemdano@gmail.com>
2022-11-26 18:24:52 -0500

    Simple add activities

--- SENG564/TimeToGo/NewStopPage.xaml.cs
+++ SENG564/TimeToGo/NewStopPage.xaml.cs
@@ -2,13 +2,43 @@ namespace TimeToGo;
 
 public partial class NewStopPage : ContentPage
 {
-	public NewStopPage()
+    public NewStopPage()
 	{
 		InitializeComponent();
-	}
-
+    }
+    
     private async void Button_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PopModalAsync();
+        if (string.IsNullOrWhiteSpace(entName.Text))
+        {
+            Error("Name is required");
+            return;
+        }
+
+        var act = new AdventureAcitivity();
+        act.Name = entName.Text;
+        act.Location = entLocation.Text;
+        if (radDuration.IsChecked)
+            act.Duration = tim.Time;
+        else if (radStart.IsChecked)
+            act.Start = dat.Date.Add(tim.Time);
+        else if (radEnd.IsChecked)
+            act.End = dat.Date.Add(tim.Time);
+        await Navigation.PopModalAsync();
+    }
+
+    void Error(string message)
+    {
+        staError.Text = message;
+        staError.IsVisible = !string.IsNullOrWhiteSpace(message);
+    }
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var btn = sender as RadioButton;
+        if (staTime != null)
+            staTime.Text = btn.Content as string;
+
+        if (dat != null)
+            dat.IsVisible = !radDuration.IsChecked;
     }
 }
