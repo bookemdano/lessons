4eb6979fa8bf9dbce4f75d8f2c5a7a32cd39c89a
Dan <bookemdano@gmail.com>
2023-03-30 15:31:56 -0400

    UX enhancements, disable buttons while changing and initializing, better logging, pointing fingers instead of arrows

--- WVU/ARFCon/MainWindow.xaml.cs
+++ WVU/ARFCon/MainWindow.xaml.cs
@@ -8,15 +8,19 @@ namespace ARFCon {
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int _shortChange = 3;
+        const int _longChange = 5;
+        const string _left = "👈";
+        const string _right = "👉";
         public MainWindow()
         {
             InitializeComponent();
             _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Interval = TimeSpan.FromSeconds(.1);
             _timer.Tick += Timer_Tick;
             // not started
 
-            Slow("Initializing", 3, false, ArfState.AllStop);
+            Slow("Initializing", _longChange, false, ArfState.AllStop);
         }
 
         DateTime _endAt;
@@ -25,31 +29,40 @@ namespace ARFCon {
         ArfState _nextState;
         private void Slow(string str, int seconds, bool allowCancel, ArfState state) {
             Log(str);
-
-            SetArf(ArfState.AllStop);
-
-            if (state == ArfState.Stop1 || state == ArfState.AllStop)
-                staArf2Status.Content = "comm...";
-            if (state == ArfState.Stop2 || state == ArfState.AllStop)
-                staArf1Status.Content = "comm...";
             
+            if (allowCancel)
+                SetArf(ArfState.AllStop);
+            prg.Value = 0;
+            prg.Maximum = seconds;
+            if (state == ArfState.Stop1 || state == ArfState.AllStop)
+                staArf2Status.Text = "comm...";
+            if (state == ArfState.Stop2 || state == ArfState.AllStop)
+                staArf1Status.Text = "comm...";
+            Buttonability(false);
             _nextState = state;
             staChanging.Text = str;
             btnCancel.Visibility = allowCancel?Visibility.Visible:Visibility.Hidden;
-            staRemainig.Text = $"{seconds} seconds remaining";
+            staRemaining.Text = $"{seconds} seconds remaining";
             pnlChanging.Visibility = Visibility.Visible;
             _endAt = DateTime.Now.AddSeconds(seconds);
             _timer.Start();
         }
-
+        private void Buttonability(bool enable) {
+            btnSwitch.IsEnabled = enable;
+            btnSwitch1.IsEnabled = enable;
+            btnSwitch2.IsEnabled = enable;
+        }
         private void Timer_Tick(object? sender, EventArgs e)
         {
-            var remaining = (int) ((_endAt - DateTime.Now).TotalSeconds);
-            staRemainig.Text = $"{remaining} seconds remaining";
-            if (remaining == 0)
+            var remaining = ((_endAt - DateTime.Now).TotalSeconds);
+            staRemaining.Text = $"{remaining.ToString("0.0")} seconds remaining";
+            prg.Value = prg.Maximum - remaining;
+
+            if (remaining <= 0)
             {
                 _timer.Stop();
                 pnlChanging.Visibility = Visibility.Hidden;
+                Buttonability(true);
                 SetArf(_nextState);
 
             }
@@ -66,27 +79,27 @@ namespace ARFCon {
                 imgArf1Stop.Visibility = Visibility.Visible;
                 imgArf2Stop.Visibility = Visibility.Visible;
                 btnSwitch.Visibility = Visibility.Hidden;
-                btnSwitch1.Content = "⬅️ Slow";
-                btnSwitch2.Content = "Slow ➡️";
+                btnSwitch1.Content = _left + " Slow";
+                btnSwitch2.Content = "Slow " + _right;
             }
             else if (state == ArfState.Stop1)
             {
                 imgArf1Stop.Visibility = Visibility.Visible;
                 imgArf2Slow.Visibility = Visibility.Visible;
                 btnSwitch.Visibility = Visibility.Visible;
-                btnSwitch1.Content = "⬅️ Slow";
-                btnSwitch2.Content = "Stop ➡️";
+                btnSwitch1.Content = _left + " Slow";
+                btnSwitch2.Content = "Stop " + _right;
             }
             else if (state == ArfState.Stop2)
             {
                 imgArf1Slow.Visibility = Visibility.Visible;
                 imgArf2Stop.Visibility = Visibility.Visible;
                 btnSwitch.Visibility = Visibility.Visible;
-                btnSwitch1.Content = "⬅️ Stop";
-                btnSwitch2.Content = "Slow ➡️";
+                btnSwitch1.Content = _left + " Stop";
+                btnSwitch2.Content = "Slow " + _right;
             }
-            staArf1Status.Content = "Conn: GOOD!";
-            staArf2Status.Content = "Conn: GOOD!";
+            staArf1Status.Text = "Conn: GOOD!";
+            staArf2Status.Text = "Conn: GOOD!";
             if (_currentState != _nextState)
                 Log("STATE: " +  _nextState);
             _currentState = _nextState;
@@ -118,19 +131,19 @@ namespace ARFCon {
         private void Switch1_Click(object sender, RoutedEventArgs e)
         {
             if (_currentState == ArfState.Stop1 || _currentState == ArfState.AllStop)
-                Slow($"Switching {_camera2} to {_slow}", 5, true, ArfState.Stop2);
+                Slow($"Switching {_camera2} to {_slow}", _shortChange, true, ArfState.Stop2);
             else // 1 is slow, 2 is stop
-                Slow("All " + _stop, 5, true, ArfState.AllStop);     // dont set other to slow because it might not be intended
+                Slow("All " + _stop, _shortChange, true, ArfState.AllStop);     // dont set other to slow because it might not be intended
         }
 
-        static string _slow = "🟨SLOW";
-        static string _stop = "🛑STOP";
+        static string _slow = "🐌 SLOW";
+        static string _stop = "⛔ STOP";
         static string _camera1 = "Cam#1 North";
         static string _camera2 = "Cam#2 North";
         private void Switch2_Click(object sender, RoutedEventArgs e)
         {
             if (_currentState == ArfState.Stop2 || _currentState == ArfState.AllStop)
-                Slow($"Switching {_camera1} to {_slow}", 5, true, ArfState.Stop1);
+                Slow($"Switching {_camera1} to {_slow}", _shortChange, true, ArfState.Stop1);
             else  // 2 is slow, 1 is stop
                 Slow("All " + _stop, 5, true, ArfState.AllStop);    // dont set 1 to slow because it might not be intended
         }
@@ -138,9 +151,9 @@ namespace ARFCon {
         private void Switch_Click(object sender, RoutedEventArgs e)
         {
             if (_currentState == ArfState.Stop2)
-                Slow($"Switching {_camera2} to {_slow}", 5, true, ArfState.Stop1);
+                Slow($"Switching {_camera2} to {_slow}", _shortChange, true, ArfState.Stop1);
             else
-                Slow($"Switching {_camera1} to {_slow}", 5, true, ArfState.Stop2);
+                Slow($"Switching {_camera1} to {_slow}", _shortChange, true, ArfState.Stop2);
         }
     }
 }
